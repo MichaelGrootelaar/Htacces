73c6ca1025e2c23efe2e2db094c6ac10d37f689b
root <m13grootelaar@gmail.com>
2017-01-19 11:34:34 +0100

    commit

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
   require_once('class/test.php');
-  
-  call_user_func(array($_GET['class'], $_GET['action']));
+
+  function test($string) {
+    $array = explode("/", $string);
+    array_splice($array, 0, 1);
+
+    call_user_func($array);
+  }
+
+  test($_SERVER['PATH_INFO']);
+
 ?>
